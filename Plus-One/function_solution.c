a7ddf5b411e9d14eb1cefda5100e964779f478b2
tunashred <alex@DESKTOP-9MAS1VF>
2023-04-19 17:23:52 +0300

    Fixed some redundancies

--- Plus-One/function_solution.c
+++ Plus-One/function_solution.c
@@ -2,28 +2,22 @@ int* plusOne(int* digits, int digitsSize, int* returnSize) {
     *returnSize = digitsSize;
     int count = 0;
     int i;
-    for(i = digitsSize - 1; i >= 0; i--) {  
-        if(digits[i] == 9) {
-            count++;
-        }
-        else {
-            break;
-        }
+    int *d_digits;
+    for(i = digitsSize - 1; i>= 0 && digits[i] == 9; i--) {  
+        count++;
     }
 
     if(count == digitsSize) {
         ++*returnSize;
+        d_digits = (int*) malloc(sizeof(int) * (*returnSize));
+        d_digits[0] = 1;
     }
     else {
         digits[digitsSize - count - 1]++;
-    }
-    int *d_digits = (int*) malloc(sizeof(int) * (*returnSize));
-    if(count != digitsSize) {
+        d_digits = (int*) malloc(sizeof(int) * (*returnSize));
         memcpy(d_digits, digits, *returnSize * sizeof(int));
     }
-    else {
-        d_digits[0] = 1;
-    }
+
     i = *returnSize - count;
     memset(d_digits + i, 0, sizeof(int) * count);
 
